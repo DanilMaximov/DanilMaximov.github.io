d3b8aedbf858f957e9a733182ec7096212961f3b
Nerbyk <danil.maximov2000@gmail.com>
2020-09-24 17:55:28 -0400

    react bootstrap included

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Home from './components/Home'
 import About from './components/About'
 import Resume from './components/Resume'
+import Works from './components/Works'
 export default function App(){
     return (
         <>
            <Home />
            <About />
            <Resume />
+           <Works />
        </>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next';
-import { GrLinkedinOption } from 'react-icons/gr'
-import { FaTelegramPlane } from 'react-icons/fa'
-import { GoMarkGithub } from 'react-icons/go'
+import { GrLinkedinOption } from 'react-icons/gr';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { GoMarkGithub } from 'react-icons/go';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default function Home(){
     const {t , i18n} = useTranslation();
@@ -11,29 +13,27 @@ export default function Home(){
       }
     return(
         <header id="home">
-            <nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#home">{t('nav.home')}</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">{t('nav.about')}</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#resume">{t('nav.resume')}</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#works">{t('nav.works')}</a>
-                        </li>
-                        <li className="nav-item">
-                            <select className="language-select" onChange= {(e) => changeLanguage(e.target.value)}>
-                                <option value="en">English</option>
-                                <option value="cz">Czech</option>
-                                <option value="ru">Russian</option>
-                            </select>
-                        </li>
-                    </ul>
-            </nav>
+            <div className="react-nav center">
+                <Navbar dark expand="lg" fixed="top">
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav center" />
+                    <Navbar.Collapse id="light-navbar-nav center">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home"><span>{t('nav.home')}</span></Nav.Link>
+                            <Nav.Link href="#about"><span>{t('nav.about')}</span></Nav.Link>
+                            <Nav.Link href="#resume"><span>{t('nav.resume')}</span></Nav.Link>
+                            <Nav.Link href="#works"><span>{t('nav.works')}</span></Nav.Link>
+                            <Nav className="language-select" >
+                                <select onChange= {(e) => changeLanguage(e.target.value)}>
+                                    <option value="en">English</option>
+                                    <option value="cz">Czech</option>
+                                    <option value="ru">Russian</option>
+                                </select>
+                            </Nav>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
             <div className="banner">
                 <div className="banner-text">
                 <h1 className="display-1">{t('home.title')}</h1>
