df5063eff3a2b844a6a3c911966ca6ea9bca8a6d
Nerbyk <danil.maximov2000@gmail.com>
2020-09-24 14:02:27 -0400

    resume component draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home'
 import About from './components/About'
+import Resume from './components/Resume'
 export default function App(){
     return (
         <>
            <Home />
            <About />
+           <Resume />
        </>
     )
 }
